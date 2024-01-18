e383e2ae697310e1b93bc768c4776ae30f0aefbd
Jayson Dasher <58889274+jaysondasher@users.noreply.github.com>
2024-01-17 22:24:15 -0500

    Initial commit

---
+++ Game Tracker/Game_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_TrackerApp.swift
+//  Game Tracker
+//
+//  Created by Jayson Dasher on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Game_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
